1603bfe6882c3a184cf8b35be521113390292ead
pichan321 <pchan@ithaca.edu>
2024-11-30 08:46:20 -0500

    test

--- tests/revoke_and_secure_test.go
+++ tests/revoke_and_secure_test.go
@@ -2,34 +2,72 @@ package tests
 
 import (
 	"blockchain-fileshare/entities"
-	"fmt"
+	"blockchain-fileshare/ipfs"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+const TEST_FILEPATH = "sample.txt"
+const GROUP_ONE_MEMBER_COUNT = 1
 
 func TestRevokeAndSecureFiles(t *testing.T) {
 	groupOwner := entities.CreateAGroupOwner()
 
-	// blockchain := entities.CreateBlockChain()
-	// sh, _ := ipfs.InitIPFS()
+	blockchain := entities.CreateBlockChain()
+	sh, _ := ipfs.InitIPFS()
 	proxy := entities.CreateIPFSProxy()
 
-	// operator := entities.CreateOperator(proxy, sh, blockchain)
-
-	groupOneMembers := []entities.Member{}
-
-	for i := 0; i < 5; i++ {
-		groupOneMembers = append(groupOneMembers, entities.CreateAGroupMember())
-	}
+	operator := entities.CreateOperator(proxy, sh, blockchain)
 
 	_, err := groupOwner.ListFiles("123")
+	assert.EqualError(t, err, "unable to locate files")
 
 	groupOneUuid := groupOwner.RegisterNewGroup(proxy)
 
 	emptyFiles, err := groupOwner.ListFiles(groupOneUuid)
-	if err != nil {
 
+	assert.Nil(t, err)
+	assert.Equal(t, len(emptyFiles), 0)
+
+	groupOneMembers := []entities.GroupMember{}
+
+	for i := 0; i < GROUP_ONE_MEMBER_COUNT; i++ {
+		groupOneMembers = append(groupOneMembers, entities.CreateAGroupMember())
+		_, _, err := groupOneMembers[i].UploadFile(&operator, &groupOwner, groupOneUuid, TEST_FILEPATH)
+		assert.EqualError(t, err, "is not a member")
+
+		groupOwner.AddNewMemberObj(proxy, groupOneUuid, groupOneMembers[i])
+
+		assert.Equal(t, groupOneMembers[i].IsMember(), true)
+		assert.NotEqual(t, groupOneMembers[i].GetUuid(), "")
+		assert.NotNil(t, groupOneMembers[i].GetPublicKey())
+
+		_, _, err = groupOneMembers[i].UploadFile(&operator, &groupOwner, "123", TEST_FILEPATH)
+		assert.EqualError(t, err, "is not a member")
+
+		transactionID, IPFSHandle, err := groupOneMembers[i].UploadFile(&operator, &groupOwner, groupOneUuid, TEST_FILEPATH)
+		assert.NotEqual(t, transactionID, "")
+		assert.NotEqual(t, IPFSHandle, "")
+		assert.Nil(t, err)
+
+		files, err := groupOwner.ListFiles(groupOneUuid)
+		assert.Nil(t, err)
+		assert.Equal(t, len(files), i + 1)
+
+		_, _, err = groupOneMembers[i].DownloadFile(&operator, groupOneUuid, "")
+		assert.EqualError(t, err, "transaction ID should not be an empty string")
+
+		_, _, err = groupOneMembers[i].DownloadFile(&operator, groupOneUuid, "abc")
+		assert.EqualError(t, err, "could not locate transaction")
+
+		_, _, err = groupOneMembers[i].DownloadFile(&operator, groupOneUuid, transactionID)
+		assert.EqualError(t, err, "could not locate transaction")
+	
 	}
 
-	fmt.Println(emptyFiles)
+	for i := 0; i < GROUP_ONE_MEMBER_COUNT; i++ {
+
+	}
 
 }
